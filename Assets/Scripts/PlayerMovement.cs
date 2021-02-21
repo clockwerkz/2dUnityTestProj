2ba8bcafedeab0887e82e5107153b006c240ea76
Carlos <clockwerkz@gmail.com>
2021-02-20 18:27:04 -0800

    Adds jump mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpForce = 20;
+    public Transform feet;
+    public LayerMask groundLayer;
 
     private Rigidbody2D _rigidbody2d;
     private float _mx;
@@ -19,6 +22,10 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         _mx = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            Jump();
+        }
     }
 
     // Update is called once per frame
@@ -26,5 +33,17 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movement = new Vector2(_mx * moveSpeed, _rigidbody2d.velocity.y);
         _rigidbody2d.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(_rigidbody2d.velocity.x, jumpForce);
+        _rigidbody2d.velocity = movement;
+    }
+
+    bool IsGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.5f, groundLayer);
+        return groundCheck != null;
     }
 }
